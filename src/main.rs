b562944b6580e74037e7c2934ad18c28e336b18a
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2022-06-04 11:07:47 +0200

    feat: add output file flag, CLI options docstrings

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,13 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
+    /// Source Brainfuck file path
     #[clap(short, long)]
     file: String,
+
+    /// If specified, compile program to file at given path (requires llvm)
+    #[clap(short, long)]
+    output: Option<String>,
 }
 
 fn main() -> std::io::Result<()> {
@@ -18,6 +23,10 @@ fn main() -> std::io::Result<()> {
 
     let ast = parser::parse(&args.file)?;
     runner::run(&ast);
+
+    if let Some(output) = &args.output {
+        dbg!(output);
+    }
 
     Ok(())
 }
